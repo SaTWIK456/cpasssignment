2d110542c48fc4d389378a95bc3094ff20f50c2c
hp <hp@satwik>
2022-03-22 18:32:16 +0530

    updated code

--- java-assignment/src/StudentClassDemo.java
+++ java-assignment/src/StudentClassDemo.java
@@ -1,4 +1,10 @@
 
 public class StudentClassDemo {
 
-}
+		public static void main(String args[]) {
+			Student st1= new Student(98,"Satwik", 21);
+			Student st2= new Student(98,"Satwik", 21);
+			System.out.println(st1.isEqual(st2));
+			
+		}
+	}
